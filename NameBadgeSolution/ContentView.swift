87b437a7b9dde9c391a67b5872efd33d4e9bacd2
Russell Gordon <rgordon@lcs.on.ca>
2022-11-08 14:23:00 -0500

    Got the text views to show vertically on top of each other.

--- NameBadgeSolution/ContentView.swift
+++ NameBadgeSolution/ContentView.swift
@@ -10,11 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("HELLO")
+            
+            Text("my name is")
+            
+            Text("Russ")
         }
+        .border(Color.red)
         .padding()
     }
 }
